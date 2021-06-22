f4e00c18de2c3a181f8d102b0999074d102dacd3
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-06-22 16:28:05 +0200

    Create pdf__cracker.py

---
+++ pdf__cracker.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import filedialog, messagebox
+import os
+
+class App:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry('700x400')
+
+        self.root.mainloop()
+
+if __name__=='__main__':
+    App()
