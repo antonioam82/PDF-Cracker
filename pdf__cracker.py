bfedf2d74f6254ee8dbd7291d93347ca9d43e958
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-06-23 10:45:21 +0200

    Update pdf__cracker.py

--- pdf__cracker.py
+++ pdf__cracker.py
@@ -8,6 +8,12 @@ class App:
         self.root.title('PDF CRACKER')
         self.root.geometry('700x400')
 
+        self.currentDir = StringVar()
+        self.currentDir.set(os.getcwd())
+
+        self.entry = Entry(self.root,textvariable=self.currentDir,width=116)
+        self.entry.place(x=0,y=0)
+
         self.root.mainloop()
 
 if __name__=='__main__':
