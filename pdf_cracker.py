5fc636a511602787716e01ab561b633da2d81ae7
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-06-27 17:33:23 +0200

    Update pdf_cracker.py

--- pdf_cracker.py
+++ pdf_cracker.py
@@ -10,14 +10,17 @@ class App:
     def __init__(self):
         self.root = Pmw.initialise(fontScheme = 'pmw1')
         self.root.title('PDF CRACKER')
-        self.root.geometry('779x480')
+        self.root.geometry('945x480')
 
-        self.main_display = Pmw.ScrolledText(self.root,text_width=64,text_height=20)
-        self.main_display.place(x=10,y=0)
+        self.main_display = Pmw.ScrolledText(self.root,text_width=62,text_height=20,hscrollmode='none')
+        self.main_display.place(x=10,y=25)
+        self.second_display = Pmw.ScrolledText(self.root,text_width=36,text_height=23,vscrollmode='dynamic')
+        self.second_display.place(x=599,y=25)
 
         self.root.mainloop()
 
 if __name__=='__main__':
     App()
+
 
 
